63b08997db8d56321e04c97870df46e6a4005802
melany orozco <mejileon11@gmail.com>
2025-10-08 12:21:50 -0600

    se agrego la recuperacion de marcos

---
+++ recuperacionmarcos/proyectomejorado/src/main/java/com/mycompany/proyectomejorado/Proyectomejorado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectomejorado;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyectomejorado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
